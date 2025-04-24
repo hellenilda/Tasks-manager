be3c3d4864da6cd6a4d3b53fe5ec20e18a1abded
hellenilda <hellen95limaaraujo@gmail.com>
2025-04-24 14:49:09 -0300

    :tada: init: primeiro commit

---
+++ src/test/java/com/hellenilda/tasksmanager/TasksmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellenilda.tasksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
